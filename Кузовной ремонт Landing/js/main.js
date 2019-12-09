c7be4641046a810b3e433dcbfe6638c6e43c663d
Sirius077 <57046438+Sirius077@users.noreply.github.com>
2019-12-10 00:41:25 +0500

    Revert "made up block 'features'"
    This reverts commit 6ba092e63c3ff4bec159fff94f041095fad26435.

--- Кузовной ремонт Landing/js/main.js
+++ Кузовной ремонт Landing/js/main.js
@@ -1,17 +1,4 @@
 $(function () {
     $("#phone").mask("7 (999) 999-9999");
-    $('.feedback-slider').slick({
-    dots: true,
-    slidesPerRow: 2,
-    rows: 2,
-    responsive: [
-    {
-      breakpoint: 478,
-      settings: {
-        slidesPerRow: 1,
-        rows: 1,
-      }
-    }
-  ]
-});
+    $('select').selectize(options);
 });
