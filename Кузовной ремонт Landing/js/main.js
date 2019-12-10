61ce31e0badbb5e2651ef5d8f1311ef10a8668bc
Sirius077 <57046438+Sirius077@users.noreply.github.com>
2019-12-11 00:38:31 +0500

    made up 'review-slider'

--- Кузовной ремонт Landing/js/main.js
+++ Кузовной ремонт Landing/js/main.js
@@ -1,15 +1,30 @@
 $(function () {
     $("#phone").mask("7 (999) 999-9999");
-    $('.feedback-slider').slick({
-    dots: true,
+    $('.review-slider').slick({
+    dots:true,
+    infinite:true,
+    sliesToScroll:1,
+    speed:400,
+    autoplay: true,
+    autoplaySpeed: 3000,
     slidesPerRow: 2,
     rows: 2,
-    responsive: [
-    {
-      breakpoint: 478,
+    responsive: [{
+      breakpoint:991,
+        settings:{
+          slidesPerRow:1,
+          rows:2,
+          prevArrow: "<button class='prev arrow'></button>",
+          nextArrow: "<button class='next arrow'></button>",
+        }
+      },
+      {
+      breakpoint: 767,
       settings: {
         slidesPerRow: 1,
         rows: 1,
+        prevArrow: "<button class='prev arrow'></button>",
+        nextArrow: "<button class='next arrow'></button>",
       }
     }
   ]
