86c78c668b9c6a2933568e84093ea5b0a41753db
Sirius077 <57046438+Sirius077@users.noreply.github.com>
2019-12-22 23:55:36 +0500

    made blocks 'Items', 'Delivery'

---
+++ Продажа Белазов Landing/js/main.js
@@ -0,0 +1,26 @@
+$(function () {
+    $('.delivery-features-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    prevArrow: "<button class='prev arrow'></button>",
+                    nextArrow: "<button class='next arrow'></button>",
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    prevArrow: "<button class='prev arrow'></button>",
+                    nextArrow: "<button class='next arrow'></button>",
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
