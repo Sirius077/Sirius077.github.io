d12e1a4c75879a3bb945c9d14f34973b3d3c95ef
Sirius077 <57046438+Sirius077@users.noreply.github.com>
2019-12-24 23:19:27 +0500

    made blocks 'convenience', 'storage', 'service'

--- Продажа Белазов Landing/js/main.js
+++ Продажа Белазов Landing/js/main.js
@@ -1,5 +1,29 @@
 $(function () {
-    $('.delivery-features-slider').slick({
+    $('#slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    prevArrow: "<button class='prev arrow'></button>",
+                    nextArrow: "<button class='next arrow'></button>",
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    prevArrow: "<button class='prev arrow'></button>",
+                    nextArrow: "<button class='next arrow'></button>",
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.storage-slider').slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
